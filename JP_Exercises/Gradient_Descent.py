a9ba7705196047d23a0a15bbb1fb843e90544aee
Gabriel Sammut <gabsamm@gmail.com>
2017-11-09 09:44:22 +0000

    Solved Gradient Descent

--- JP_Exercises/Gradient_Descent.py
+++ JP_Exercises/Gradient_Descent.py
@@ -1,21 +1,35 @@
-import math
+import numpy as np
 import matplotlib.pyplot as plt
-x0,x1 = 2,-2
+import math
+#
+def parabola(x, a, b, c, d):
+    return a*x**4 - b*x**2 + c*x - d
+x0,x1 = 1.5,-1.5
+start_x0,start_x1 = x0,x1
 alpha = 0.01
-epochs = 100
+epochs = 110
 iterations = 0
 cost_function = lambda x : (16 * math.pow(x,3)) - (6 * x) + 2
 weight = lambda x : x - alpha * cost_function(x)
+#
+x = np.linspace(-2, 2, 30)
+y = parabola(x, 4, 3, 2, 1)
+plt.xlabel("P"), plt.ylabel("Cost"), plt.title("P vs Cost")
+plt.plot(x, y)
+plt.plot(x0, parabola(x0, 4, 3, 2, 1), 'ro'), plt.plot(x1, parabola(x1, 4, 3, 2, 1), 'bo'), plt.plot(x, y), plt.show()
+plt.show()
+#
 for i in range(epochs):
     temp0 = weight(x0)
     temp1 = weight(x1)
-    if temp0 <= 0 or temp1 >= 0:
-        break
     x0 = temp0
     x1 = temp1
     #
     plt.xlabel("P"), plt.ylabel("Cost"), plt.title("P vs Cost")
-    #plt.plot(x0, cost_function(x0), 'ro'), plt.plot(x1, cost_function(x1), 'bo'), plt.show()
+    plt.plot(x0, parabola(x0, 4, 3, 2, 1), 'ro'), plt.plot(x1, parabola(x1, 4, 3, 2, 1), 'bo'), plt.plot(x, y), plt.show()
     iterations += 1
-
-print("X0= " + str(x0) + " ,X1= " + str(x1) + " ,Iterations: " + str(iterations))
+else:
+    plt.xlabel("P"), plt.ylabel("Cost"), plt.title("P vs Cost")
+    plt.plot(x0, parabola(x0, 4, 3, 2, 1), 'ro'), plt.plot(x1, parabola(x1, 4, 3, 2, 1), 'bo'), plt.plot(x, y), plt.show()
+    print('Alpha (Rate)= '+str(alpha)+"\nepochs= "+str(epochs)+"\nx0= "+str(start_x0)+", x1= "+str(start_x1))
+    print("X0= " + str(x0) + " ,X1= " + str(x1) + " ,Iterations: " + str(iterations))
